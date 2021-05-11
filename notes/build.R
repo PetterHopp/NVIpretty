59bf2819331b8363ed71b83ffdc60002f4ea81e4
Petter Hopp <Petter.Hopp@vetinst.no>
2021-05-11 12:00:06 +0200

    chore: build.R corrected build and test

--- notes/build.R
+++ notes/build.R
@@ -22,11 +22,16 @@ devtools::document()
 
 # Build the package
 # system("R CMD build ../NVIpretty")
-devtools::build(binary = TRUE)
-# devtools::build(binary = TRUE, manual = TRUE, vignettes = TRUE)
+# devtools::build(binary = TRUE)
+devtools::build(binary = FALSE, manual = TRUE, vignettes = TRUE)
+
+version <- packageVersion("NVIpretty", lib.loc = paste0(getwd(),"/.."))
+# devtools::check_built(path = paste0("../NVIpretty_", version, ".tar.gz"), manual = TRUE)
+devtools::check_built(path = paste0("../NVIpretty_", version, ".tar.gz"), args = c("--no-tests"), manual = TRUE)
+
 
 # Extensive checking of package. Is done after build. Creates PDF-manual
-system("R CMD check --ignore-vignettes ../NVIpretty")
+# system("R CMD check --ignore-vignettes ../NVIpretty")
 
 # Alternative for creating the PDF-manual. The manual is not put in the correct directory
 # system(paste(shQuote(file.path(R.home("bin"), "R")),
