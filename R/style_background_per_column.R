862d4309e17ace733ab460be2192dcc7f56f5e78
Petter Hopp <Petter.Hopp@vetinst.no>
2021-09-25 00:15:46 +0200

    fix: style_background_per_column Corrected join of colname and palette. Updated help. Included argument checking.

--- R/style_background_per_column.R
+++ R/style_background_per_column.R
@@ -1,13 +1,13 @@
-#' @title Style cell background in Excel columns
-#' @description Style the cell background with different colours. The colours are 
-#'     given in the palette and the colours are named with the standard column names 
-#'     like paavist, ikke_paavist. In each column, the cells that are following the 
-#'     are styled. 
+#' @title Style cell background in Excel sheet based on column name 
+#' @description Style the cell background with different colours. The background 
+#'     colours are chosen based on the column names, for example "paavist" or 
+#'     "tvilsom". In each column, all cells that are following the given 
+#'     rule, will get the same background colour. 
 #'     
-#' @details The colours in the palette should be named with the standard column 
-#'     names corresponding to the different colours. The colname should give the 
-#'     with the standard column names. If the names of the columns in the input
-#'     data frame are not the standard column names, the colname should be named.
+#' @details The columns will be styled with the colour in the palette that have 
+#'     the same name as the column. If the column name is not the same as the 
+#'     name of the colour in the palette, the vector of column names can be named
+#'     with the corresponding colour name, see example. 
 #'
 #' @param workbook The workbook object
 #' @param sheet The Excel sheet name
@@ -33,6 +33,37 @@
 #'
 #' @author Petter Hopp Petter.Hopp@@vetinst.no
 #' @export
+#' @examples
+#' \dontrun{
+#' # Attach packages and set up with temporary directory
+#' td <- tempdir()
+#' library(NVIpretty)
+#' library(openxlsx)
+#'
+#' # Make workbook object
+#' workbook <- createWorkbook()
+#'
+#' # Add a sheet to the workbook
+#' add_formatted_worksheet(iris,
+#'                         workbook,
+#'                         sheet = "iris",
+#'                         wrapHeadlineText = TRUE,
+#'                         collabels = TRUE,
+#'                         colwidths = FALSE,
+#'                         standards = NULL)
+#'                         
+#'  style_background_per_column(workbook = workbook, 
+#'                              sheet = sheet,
+#'                              data = iris,
+#'                              rule = "> 3",
+#'                              colname = c("bluegrey" = "Sepal.Length", "bluegrey" = "Sepal.Width", "green" = "Petal.Length"),
+#'                              palette = c("bluegrey" = "#6fb2d3", "green" = "#579e65"))                       
+#'                         
+#' #Save the workbook
+#' saveWorkbook(wb = workbook, file = paste0(td, "/iris.xlsx"), overwrite = TRUE)
+#'
+#' print(paste("You may examine the Excel workbook at", normalizePath(td, win)))
+#' }
 
 style_background_per_column <- function(workbook = workbook, 
                                         sheet = sheet,
@@ -49,35 +80,36 @@ style_background_per_column <- function(workbook = workbook,
   if (is.null(nrows_in_data)) {nrows_in_data <- nrow(data)}
   
   # make table with colour and columnname
-  colname <- set_name_vector(colname)
-  colours <- cbind(names(colname), unname(palette[colname]))
+  colname <- NVIpretty:::set_name_vector(colname)
+  colours <- cbind(unname(colname), unname(palette[names(colname)]))
   colnames(colours) <- c("colname", "colour") 
   
   # ARGUMENT CHECKING ----
   # Object to store check-results
   checks <- checkmate::makeAssertCollection()
-  
+
   # Perform checks
   checkmate::assert_class(workbook, classes = "Workbook", add = checks)
   checkmate::assert_character(sheet, len = 1, min.chars = 1, add = checks)
   checkmate::assert_data_frame(data, null.ok = TRUE, add = checks)
-  # checkmate::assert_character(colnames_in_data, add = checks)
-  # checkmate::assert_integerish(nrows_in_data, max.rows = (1048576 - 1), max.cols = 16384, add = checks)
-  # checkmate::assert_character(rule, add = checks)
-  # checkmate::assert_character(colname, add = checks)
-  # checkmate::assert_character(palette, add = checks)
-  # NVIcheckmate::assert(checkmate::check_logical(colwidths),
-  #                      checkmate::check_choice(colwidths, choices = "auto"),
-  #                      combine = "or",
-  #                      add = checks)
-  
+  checkmate::assert_character(colnames_in_data, min.len = 1, null.ok = TRUE, add = checks)
+  checkmate::assert_integerish(nrows_in_data, lower = 2, len = 1, null.ok = TRUE, add = checks)
+  NVIcheckmate::assert_non_missing(x = list(data, colnames_in_data), add = checks)
+  NVIcheckmate::assert_non_missing(x = list(data, nrows_in_data), add = checks)
+  checkmate::assert_character(rule, len = 1, add = checks)
+  checkmate::assert_character(colname, min.len = 1, add = checks)
+  checkmate::assert_character(palette, min.len = 1, add = checks)
+  NVIcheckmate::assert_character(x = intersect(names(colname), names(palette)), min.len = 1,
+                                 comment = "At least one column must be associated with a colour in the palette",
+                                 add = checks)
+
   # Report check-results
   checkmate::reportAssertions(checks)
   
   # RUNNING SCRIPT ----
   # Style each column with colname
   # i <- 1
-  for (i in 1:nrow(colours)) {
+  for (i in c(1:nrow(colours))) {
     openxlsx::conditionalFormatting(wb = workbook, 
                                     sheet = sheet,
                                     cols = which(colnames_in_data == colours[i, "colname"]),
