cfb6fd8693038b710a9e1eb1f541405d909415cc
Petter Hopp <Petter.Hopp@vetinst.no>
2021-09-25 20:24:33 +0200

    refactor: test_style_worksheet Included reference to openxlsx:: Styling

--- tests/testthat/test_style_worksheet.R
+++ tests/testthat/test_style_worksheet.R
@@ -34,7 +34,7 @@ test_that("format worksheet", {
   #              file = paste0(td, "/iris.xlsx"),
   #              overwrite = TRUE)
   
-  expect_identical(colnames(read.xlsx(xlsxFile = workbook, sheet = "iris", rows = 1, sep.names = " ")),
+  expect_identical(colnames(openxlsx::read.xlsx(xlsxFile = workbook, sheet = "iris", rows = 1, sep.names = " ")),
                    c("Sepal length", "Sepal width", "Petal length", "Petal width", "Species"))
   
   
@@ -59,20 +59,20 @@ test_that("include function in add_formatted_worksheet", {
                           standards = NULL,
                           FUN = style_background_per_column,
                           rule = "> 4",
-                              colname = c("PCR-positiv" = "Sepal.Width"),
-                              palette = c("PCR-positiv" = "red", "Seropositiv" = "orange", "Tvilsom" = "yellow", "Negativ" = "green"))
+                          colname = c("PCR-positiv" = "Sepal.Width"),
+                          palette = c("PCR-positiv" = "red", "Seropositiv" = "orange", "Tvilsom" = "yellow", "Negativ" = "green"))
   
   
- 
+  
   #Save the workbook
   # openxlsx::saveWorkbook(wb = workbook,
   #              file = paste0(td, "/iris.xlsx"),
   #              overwrite = TRUE)
   
-  expect_identical(colnames(read.xlsx(xlsxFile = workbook, sheet = "iris", rows = 1, sep.names = " ")),
+  expect_identical(colnames(openxlsx::read.xlsx(xlsxFile = workbook, sheet = "iris", rows = 1, sep.names = " ")),
                    c("Sepal length", "Sepal width", "Petal length", "Petal width", "Species"))
   
-
+  
   # print(paste("One I may examine the Excel workbook at", normalizePath(td, winslash = "/")))
 })
 
