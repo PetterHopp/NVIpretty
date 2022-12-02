5eeb576d75f7ab014d33e0c92c19e77cdb08dd3c
Petter Hopp <petter.hopp@vetinst.no>
2022-12-02 08:53:39 +0100

    test: extended test_append_text_line Included test for errors when checking arguments.

--- tests/testthat/test_append_text_line.R
+++ tests/testthat/test_append_text_line.R
@@ -23,3 +23,24 @@ expect_identical(y[dim(y)[1], 1], paste("Datauttrekket er gjort", format(Sys.Dat
 
 
 })
+
+test_that("Error testing of append_text_line", {
+
+  linewidth <- options("width")
+  options(width = 80)
+
+  expect_error(append_text_line(data = "vector", text = "text", empty_rows = 0),
+               regexp = "Variable 'data': Must be of type 'data.frame', not 'character'",
+               fixed = TRUE)
+
+  expect_error(append_text_line(data = mtcars, text = NULL, empty_rows = 0),
+               regexp = "Variable 'text': Must be of type 'character', not 'NULL'",
+               fixed = TRUE)
+
+  expect_error(append_text_line(data = mtcars, text = "text", empty_rows = -1),
+               regexp = "Variable 'empty_rows': Element 1 is not >= 0.",
+               fixed = TRUE)
+
+
+  options(width = unlist(linewidth))
+})
