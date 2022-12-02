3e378349f42fad564f26ac67ad69796fb1f2f054
Petter Hopp <petter.hopp@vetinst.no>
2022-12-02 08:58:39 +0100

    style: styled spaces in test_set_name_vector

--- tests/testthat/test_set_name_vector.R
+++ tests/testthat/test_set_name_vector.R
@@ -14,7 +14,7 @@ expect_equal(unname(y), names(y))
 x <- c("Aar" = "År", "MT_regionnr" = "MT regionnr", "MT region", "MT avdelingsnr", "MT avdeling")
 y <- NVIpretty:::set_name_vector(x)
 expect_equal(names(y),
-             c("Aar", "MT_regionnr", "MT region", "MT avdelingsnr", "MT avdeling") )
+             c("Aar", "MT_regionnr", "MT region", "MT avdelingsnr", "MT avdeling"))
 
 x <- c("Aar" = "År", "MT_regionnr" = "MT regionnr", "MT region" = "MT region",
        "MT avdelingsnr" = "MT avdelingsnr", "MT avdeling" = "MT avdeling")
@@ -23,4 +23,3 @@ expect_equal(names(y),
              c("Aar", "MT_regionnr", "MT region", "MT avdelingsnr", "MT avdeling"))
 
 })
-
