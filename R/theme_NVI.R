885fada849220a7a06bc5c491c17105d2efb79b0
Petter Hopp <Petter.Hopp@vetinst.no>
2025-10-14 07:36:28 +0200

    perf: updated horizontal gridline in theme_NVI

--- R/theme_NVI.R
+++ R/theme_NVI.R
@@ -74,7 +74,7 @@ theme_NVI <- function(base_size = 11, base_family = "",
                    panel.border = ggplot2::element_blank(),
                    strip.background = ggplot2::element_blank(),
                    plot.background = ggplot2::element_blank(),
-                   panel.grid.major.y = ggplot2::element_line(color = "#969FB1", size = 0.2),
+                   panel.grid.major.y = ggplot2::element_line(color = "#d9d9d9", size = 0.25),
                    panel.grid.major.x = ggplot2::element_blank(),
                    panel.grid.minor = ggplot2::element_blank(),
 
