462e82637c23dabd01fd3177682bf7eccd6a2d94
Petter Hopp <petter.hopp@vetinst.no>
2025-11-05 14:28:49 +0100

    feat: updated theme Updated theme_NVI. Created theme_NVI_report.

--- R/theme_NVI.R
+++ R/theme_NVI.R
@@ -39,45 +39,103 @@
 #' @export
 
 
-theme_NVI <- function(base_size = 11, base_family = "",
+theme_NVI <- function(base_size = 11, 
+                      base_family = "",
                       base_line_size = base_size / 22,
-                      base_rect_size = base_size / 22) {
+                      base_rect_size = base_size / 22,
+                      legend.position = 'bottom',
+                      ink = "black",
+                      paper = "white") {
   # Starts with theme_grey and then modify some parts
   ggplot2::theme_grey(
     base_size = base_size,
     base_family = base_family,
     base_line_size = base_line_size,
-    base_rect_size = base_rect_size
+    base_rect_size = base_rect_size,
+    ink = ink,
+    paper = paper
   ) %+replace%
-    ggplot2::theme(line = ggplot2::element_line(colour = "#091A3E",
+    ggplot2::theme(line = ggplot2::element_line(colour = ink,
                                                 size = base_line_size,
                                                 linetype = 1,
                                                 lineend = "butt"),
 
-                   rect = ggplot2::element_rect(fill = "#FFFFFF",
-                                                colour = "#091A3E",
+                   rect = ggplot2::element_rect(fill = paper, 
+                                                colour = ink,
                                                 size = base_rect_size,
                                                 linetype = 1),
 
                    text = ggplot2::element_text(family = base_family,
                                                 face = "plain",
-                                                colour = "#091A3E",
+                                                colour = ink,
                                                 size = base_size,
                                                 lineheight = 0.9, hjust = 0.5, vjust = 0.5, angle = 0,
                                                 margin = ggplot2::margin(),
                                                 debug = FALSE),
 
-                   axis.ticks = ggplot2::element_blank(),
+                   # axis.ticks = ggplot2::element_blank(),
                    legend.background = ggplot2::element_blank(),
                    legend.key = ggplot2::element_blank(),
-                   panel.background = ggplot2::element_blank(),
-                   panel.border = ggplot2::element_blank(),
+                   legend.position = legend.position,
+                   legend.title = element_blank(),
+                   # panel.background = ggplot2::element_blank(),
+                   panel.border = ggplot2::element_rect(fill = NA),
                    strip.background = ggplot2::element_blank(),
-                   plot.background = ggplot2::element_blank(),
+                   # plot.background = ggplot2::element_blank(),
                    panel.grid.major.y = ggplot2::element_line(color = "#d9d9d9", size = 0.25),
                    panel.grid.major.x = ggplot2::element_blank(),
                    panel.grid.minor = ggplot2::element_blank(),
 
+                   complete = TRUE
+    )
+}
+
+
+
+theme_NVI_report <- function(
+    base_size = 8, 
+    base_family = "calibri",
+    ink = "black",
+    paper = "white",
+    line_size = 0.5,
+    rect_size = 0.5,
+    axis_text_x = element_text(family = "calibri-light", face = "plain", size = 6, angle = 0),
+    axis_text_y = element_text(family = "calibri-light", face = "plain", size = 6),
+    axis_title_x_family = element_blank(),
+    axis_title_y = element_text(family = "calibri", face = "plain", size = 8),
+    legend_position = 'bottom',
+    legend_text = element_text(family = "calibri-light", face = "plain", size = 6)
+) {
+  # Starts with theme_grey and then modify some parts
+  ggplot2::theme_NVI(
+    base_size = base_size,
+    base_family = base_family,
+    base_line_size = base_line_size,
+    base_rect_size = base_rect_size,
+    ink = ink,
+    paper = paper
+  ) %+replace%
+    ggplot2::theme(text = ggplot2::element_text(family = base_family,
+                                                face = "plain",
+                                                colour = ink,
+                                                size = base_size,
+                                                lineheight = 0.9, hjust = 0.5, vjust = 0.5, angle = 0,
+                                                margin = ggplot2::margin(),
+                                                debug = FALSE),
+                   
+                   # axis.ticks = ggplot2::element_blank(),
+                   legend.background = ggplot2::element_blank(),
+                   legend.key = ggplot2::element_blank(),
+                   legend.position = legend_position,
+                   legend.title = element_blank(),
+                   # panel.background = ggplot2::element_blank(),
+                   panel.border = ggplot2::element_rect(fill = NA),
+                   strip.background = ggplot2::element_blank(),
+                   # plot.background = ggplot2::element_blank(),
+                   panel.grid.major.y = ggplot2::element_line(color = "#d9d9d9", size = 0.25),
+                   panel.grid.major.x = ggplot2::element_blank(),
+                   panel.grid.minor = ggplot2::element_blank(),
+                   
                    complete = TRUE
     )
 }
